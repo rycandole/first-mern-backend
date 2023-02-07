011400bb04c1bf66cb44512ae3f55f57cb5469d1
Ryan C <ry.candole@gmail.com>
2023-02-08 00:02:10 +0800

    fix/server

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const express = require('express');
-const mongoose = require('mongoose');
-const workoutRoute = require('./routes/workouts');
-const userRoute = require('./routes/user');
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
 
 // middleware
 app.use(express.json())
+
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
 
 // routes
-app.use('/api/workouts', workoutRoute)
-app.use('/api/user', userRoute)
+app.use('/api/workouts', workoutRoutes)
 
 // connect to db
-mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONG_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    // listen for requests
+    console.log('connected to database')
+    // listen to port
     app.listen(process.env.PORT, () => {
-      console.log('Connected to db & listening on port', process.env.PORT)
+      console.log('listening for requests on port', process.env.PORT)
     })
   })
-  .catch((error) => {
-    console.log(error)
-  })
+  .catch((err) => {
+    console.log(err)
+  }) 
